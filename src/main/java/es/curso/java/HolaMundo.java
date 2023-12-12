a20399e337b79de677ced17b94e82fa0022b591b
Jose Luis Getafe <email@email.es>
2023-12-12 19:23:40 +0100

    Inicio Proyecto

---
+++ src/main/java/es/curso/java/HolaMundo.java
@@ -0,0 +1,16 @@
+package es.curso.java;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+public class HolaMundo {
+	private static final Logger logger = LogManager.getLogger(HolaMundo.class);
+	
+	public static void main(String[] args) {
+		System.out.println("Hola Mundo " + args[0]);
+		logger.info("Info");
+
+	}
+
+}
