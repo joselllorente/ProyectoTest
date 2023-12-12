97032eeaedf2d10c5bca238074f7044f2bb1feae
Jose Luis <joseluis.llorenteperales@gmail.com>
2023-12-12 20:05:14 +0100

    Update HolaMundo.java
    Cambio en remoto

--- src/main/java/es/curso/java/HolaMundo.java
+++ src/main/java/es/curso/java/HolaMundo.java
@@ -8,7 +8,7 @@ public class HolaMundo {
 	private static final Logger logger = LogManager.getLogger(HolaMundo.class);
 	
 	public static void main(String[] args) {
-		System.out.println("Hola Mundo 2" + args[0]);
+		System.out.println("Hola Mundo 3" + args[0]);
 		logger.info("Info");
 
 	}
